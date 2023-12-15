d79d827008c072d293d8e5cdb6d528229981ad22
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-15 17:22:06 -0300

    :art: Add Game Window Grid

--- game.py
+++ game.py
@@ -5,7 +5,22 @@ class Game:
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
 
+    def draw_game_grid(self) -> None:
+        for column in range(1, COLUMNS):
+            i = 0
+            j = column * CELL_SIZE
+            pygame.draw.line(self.surface, COLORS['GAME_LINE_COLOR'],
+                            start_pos=(j, i), end_pos=(j, self.surface.get_height()), width=1)
+        
+        for row in range(1, ROWS):
+            i = row * CELL_SIZE
+            j = 0
+            pygame.draw.line(self.surface, COLORS['GAME_LINE_COLOR'],
+                            start_pos=(j, i), end_pos=(self.surface.get_width(), i), width=1)
+    
+
     def run(self) -> None:
+        self.draw_game_grid()
         self.display_surface.blit(self.surface, (2 * WINDOW_PADDING + LEFT_SIDEBAR_WIDTH, WINDOW_PADDING))
         self.surface.fill(COLORS['GAME_BACKGROUND'])
 
